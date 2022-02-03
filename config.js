fadfc8c1ebf36f5eadc73146bee75058a537d9dd
Snigdhadipm <85871995+Snigdhadipm@users.noreply.github.com>
2022-02-03 14:45:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCexuI3nZKJ4Lm1q7bntbfTTsLs9ndG698",
+  authDomain: "team-voting-app-4212f.firebaseapp.com",
+  databaseURL: "https://team-voting-app-4212f-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-4212f",
+  storageBucket: "team-voting-app-4212f.appspot.com",
+  messagingSenderId: "679218496365",
+  appId: "1:679218496365:web:657d0b20da06e651611064",
+  measurementId: "G-8RZ7C18WVK"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
